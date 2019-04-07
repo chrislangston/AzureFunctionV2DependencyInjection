132a3b921a83b8e2a01abd2c6cd261341b230dfa
Chris Langston <clangston@capaxglobal.com>
2019-04-06 22:28:31 -0400

    First Commit

---
+++ PhoneClaimsFunctionApp/BlobTriggeredInboundPhoneClaimFunction.cs
@@ -0,0 +1,16 @@
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace PhoneClaimsFunctionApp
+{
+    public static class BlobTriggeredInboundPhoneClaimFunction
+    {
+        [FunctionName("BlobTriggeredInboundPhoneClaimFunction")]
+        public static void Run([BlobTrigger("inbound-phone-claims/{name}", Connection = "StorageConnectionSettings")]Stream myBlob, string name, ILogger log)
+        {
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+        }
+    }
+}
---
+++ PhoneClaimsFunctionApp/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+using PhoneClaimsFunctionApp;
+
+[assembly: WebJobsStartup(typeof(Startup))]
+namespace PhoneClaimsFunctionApp
+{
+    public class Startup : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+}
