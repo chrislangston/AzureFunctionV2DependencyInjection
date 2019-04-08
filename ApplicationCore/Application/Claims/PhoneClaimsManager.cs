f41aad43e653196ecd6b733f197ff5a30ed29ced
Chris Langston <clangston@capaxglobal.com>
2019-04-07 22:25:56 -0400

    Model and ClaimsManager.

---
+++ ApplicationCore/Application/Claims/PhoneClaimsManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using ApplicationCore.Application.Interfaces.Claims;
+
+namespace ApplicationCore.Application.Claims
+{
+    public class PhoneClaimsManager : IPhoneClaimsManager
+    {
+
+        public async Task<bool> ManageClaimAsync(PhoneClaimModel model)
+        {
+            return await Task.FromResult(true);
+        }
+    }
+}
